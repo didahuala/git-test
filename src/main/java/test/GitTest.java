871cbbb387ce3967c5145c74d68a5aa5269c3b67
rzw <2711099842@qq.com>
2023-08-07 17:20:47 +0800

    first commit

---
+++ src/main/java/test/GitTest.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author RZW
+ * @create 2023-08-07-17:07-
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git-test");
+    }
+}
